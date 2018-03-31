fa8baf85386d4ec8344f6238422b43c767a0f6a5
Hadrián Montes <hadrianmontes@gmail.com>
2018-03-31 18:42:50 +0200

    Added loading of previous log

--- weather_station.py
+++ weather_station.py
@@ -1,4 +1,5 @@
 import time
+import os.path
 from collections import deque
 from bme280 import BME280
 from matplotlib import dates
@@ -11,12 +12,7 @@ class WeatherStation(object):
                  stdout_print=True, **kwargs):
         self.sensor = BME280(**kwargs)
         self._update_time = update_time
-        if logfile is not None:
-            self._logfile = open(logfile, "w+")
-        else:
-            self._logfile = None
         self._databuffer = deque(maxlen=maxbuffer)
-
         self.date_format = "%d-%m-%Y %H:%M:%S"
         self._print = stdout_print
         self._stop = False
@@ -24,6 +20,26 @@ class WeatherStation(object):
             "temperature": None,
             "pressure": None,
         }
+
+        if logfile is not None:
+            self._open_and_load(logfile)
+        else:
+            self._logfile = None
+
+
+    def _open_and_load(self, logfile):
+        if os.path.isfile(logfile):
+            nspaces_format = len(self.date_format.split())
+            self._logfile = open(logfile, "r+")
+            for line in self._logfile:
+                line = line.split()
+                date = " ".join(line[:nspaces_format])
+                epoch = time.mktime(time.strptime(date, self.date_format))
+                temperature = float(line[nspaces_format])
+                pressure = float(line[nspaces_format + 1])
+                self._databuffer.append((epoch, temperature, pressure))
+        else:
+            self._logfile = open(logfile, "w")
 
     def run(self, maxtime=None):
         start = time.time()
